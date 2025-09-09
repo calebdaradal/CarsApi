b419fa57a9414bfdba93ecd7be10b3c9be02afae
calebdaradal <jeramydaradal@gmail.com>
2025-09-09 22:04:57 +0800

    CA-1 Feature: Create Car

---
+++ Domain/Entities/Car.cs
@@ -0,0 +1,15 @@
+﻿
+namespace Domain.Entities
+{
+    public class Car
+    {
+        public int Id { get; set; }
+        public string TeamName { get; set; }
+        public int Speed { get; set; }
+        public double MalfunctionChance { get; set; }
+        public int MalfunctionOccured { get; set; }
+        public int DistanceCoveredInMiles { get; set; }
+        public int FinishedRace { get; set; }
+        public int RacedForHours { get; set; }
+    }
+}
